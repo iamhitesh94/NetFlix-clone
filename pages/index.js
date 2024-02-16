a62010a9bdfe797b4bd6090cf23a241fbaeefaab
iamhitesh94 <iamhitesh3@gmail.com>
2024-02-16 23:38:29 +0530

    Index page small change

--- pages/index.js
+++ pages/index.js
@@ -5,10 +5,16 @@ export default function Home() {
   const router = useRouter();
   useEffect(() => {
     router.push("/login")
-  },[])
+  }, [])
   return (
     <>
-      <h1 className="text-2xl text-green-500">Netflix</h1>
+      <div className={`relative h-[100vh] w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover`}>
+        <div className="bg-black w-full h-full lg:bg-opacity-50">
+          <nav className="px-12 py-5">
+            <img src="/images/logo.png" className="h-12" />
+          </nav>
+        </div>
+      </div>
     </>
   )
 }
