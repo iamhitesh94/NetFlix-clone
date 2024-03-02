a905bbd07c14e4f9c74558a15211bce5bbadcfc0
iamhitesh94 <iamhitesh3@gmail.com>
2024-03-02 11:21:20 +0530

    Solve issue having on build due to quotation

--- pages/404.js
+++ pages/404.js
@@ -11,7 +11,7 @@ export default function Page404() {
                             <Image src="/images/logo.png" alt="Logo" width={178} height={48} className="h-12 mb-8" />
                         </Link>
                         <h1 className="text-4xl font-bold text-white mb-4">404 - Page Not Found</h1>
-                        <p className="text-white">Sorry, the page you're looking for could not be found.</p>
+                        <p className="text-white">Sorry, the page you&apos;re looking for could not be found.</p>
                     </div>
                 </div>
             </div>
